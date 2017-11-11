345321e9f7f021d799bbf66361bd961ad89ffb8b
Mansi Ranjit Mane <mmane@weh5201-a2.andrew.cmu.edu>
2017-11-11 14:55:28 -0500

    added file q34

---
+++ code/q3_4_predict.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def main():
+    th_words = [['government', 'of', 'united'], ['city', 'of', 'new'], ['life', 'in', 'the']]
+    x = np.array()
+    #for line in th_words:
+    w2 = np.load('obj/we_mapbs_256_lr_0.05_hl_ 128_ev_16_ep_4.npy')
+
+    w2.item()
+
+
+if __name__ == '__main__':
+    main()
